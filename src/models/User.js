2760f3f07d4c290752a27791f3ca576067ba0d7d
MatTrevoso <mateusloman@gmail.com>
2020-10-20 13:29:14 -0300

    arrumando o schema

--- src/models/User.js
+++ src/models/User.js
@@ -3,10 +3,9 @@ const mongoosePaginate = require('mongoose-paginate')
 
 const UserSchema = new mongoose.Schema({
     name: String,
-    github_username: String,
-    age: Number,
-    avatar_url: String,
-    techs: [String],
+    username: String,
+    email: String,
+    techs: [String]
 })
 
 UserSchema.plugin(mongoosePaginate)
